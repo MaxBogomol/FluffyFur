54f1b66dbd8773d406a2638f033b930690708d6c
MaxBogomol <maxglotov159@gmail.com>
2024-08-29 22:40:48 +0500

    packets

---
+++ src/main/java/mod/maxbogomol/fluffy_fur/common/network/BlockEntityUpdate.java
@@ -0,0 +1,22 @@
+package mod.maxbogomol.fluffy_fur.common.network;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.server.level.ServerChunkCache;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.block.entity.BlockEntity;
+
+public class BlockEntityUpdate {
+    public static void packet(BlockEntity blockEntity) {
+        if (blockEntity != null && blockEntity.getLevel() instanceof ServerLevel) {
+            Packet<?> packet = blockEntity.getUpdatePacket();
+            if (packet != null) {
+                BlockPos pos = blockEntity.getBlockPos();
+                ((ServerChunkCache) blockEntity.getLevel().getChunkSource()).chunkMap
+                        .getPlayers(new ChunkPos(pos), false)
+                        .forEach(e -> e.connection.send(packet));
+            }
+        }
+    }
+}
