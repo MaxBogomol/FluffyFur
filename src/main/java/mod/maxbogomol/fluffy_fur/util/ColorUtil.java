5f3e9bd2cef00fba02292afd7c279f92dcdab477
MaxBogomol <maxglotov159@gmail.com>
2024-09-28 16:38:55 +0500

    blend color

--- src/main/java/mod/maxbogomol/fluffy_fur/util/ColorUtil.java
+++ src/main/java/mod/maxbogomol/fluffy_fur/util/ColorUtil.java
@@ -1,6 +1,7 @@
 package mod.maxbogomol.fluffy_fur.util;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 public class ColorUtil {
     public static int getAlpha(int packedColor) {
@@ -36,5 +37,25 @@ public class ColorUtil {
         int g = (int) (Math.sin(ticks + Math.PI / 2) * 127 + 128);
         int b = (int) (Math.sin(ticks + Math.PI) * 127 + 128);
         return new Color(r, g, b);
+    }
+
+    public static Color getBlendColor(ArrayList<Color> colors) {
+        float f = 0.0F;
+        float f1 = 0.0F;
+        float f2 = 0.0F;
+        int j = 0;
+
+        for(Color color : colors) {
+            int k = ColorUtil.packColor(color);
+            f += (float) ((k >> 16 & 255)) / 255.0F;
+            f1 += (float) ((k >> 8 & 255)) / 255.0F;
+            f2 += (float) ((k >> 0 & 255)) / 255.0F;
+            j += 1;
+        }
+
+        f = f / (float)j * 255.0F;
+        f1 = f1 / (float)j * 255.0F;
+        f2 = f2 / (float)j * 255.0F;
+        return ColorUtil.getColor((int) f << 16 | (int) f1 << 8 | (int) f2);
     }
 }
