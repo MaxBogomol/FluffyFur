f8c9a850125a75dc78156150df8361b77143c692
MaxBogomol <maxglotov159@gmail.com>
2024-11-05 20:50:38 +0500

    tags

---
+++ src/main/java/mod/maxbogomol/fluffy_fur/registry/common/damage/FluffyFurDamageTags.java
@@ -0,0 +1,10 @@
+package mod.maxbogomol.fluffy_fur.registry.common.damage;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.damagesource.DamageType;
+
+public class FluffyFurDamageTags {
+    public static final TagKey<DamageType> MAGIC = TagKey.create(Registries.DAMAGE_TYPE, new ResourceLocation("forge", "is_magic"));
+}
