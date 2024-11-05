665232d363cc3945fe619573528f65637c996f2a
MaxBogomol <maxglotov159@gmail.com>
2024-11-05 20:02:33 +0500

    silly treat

---
+++ src/main/java/mod/maxbogomol/fluffy_fur/registry/common/item/FluffyFurFoods.java
@@ -0,0 +1,7 @@
+package mod.maxbogomol.fluffy_fur.registry.common.item;
+
+import net.minecraft.world.food.FoodProperties;
+
+public class FluffyFurFoods {
+    public static final FoodProperties SILLY_TREAT = (new FoodProperties.Builder()).nutrition(5).saturationMod(0.2F).alwaysEat().build();
+}
