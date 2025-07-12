603c1e6bf37d7ae6b2783821e44d95e78abb12c3
MaxBogomol <maxglotov159@gmail.com>
2025-07-12 20:20:24 +0500

    yap

--- src/main/java/mod/maxbogomol/fluffy_fur/config/FluffyFurConfig.java
+++ src/main/java/mod/maxbogomol/fluffy_fur/config/FluffyFurConfig.java
@@ -10,7 +10,7 @@ public class FluffyFurConfig {
             VOID_HEIGHT;
 
     public FluffyFurConfig(ForgeConfigSpec.Builder builder) {
-        builder.comment("misc").push("misc");
+        builder.comment("Misc").push("misc");
         TRIDENT_LOYALTY_VOID = builder.comment("Enables invulnerability of a trident enchanted to a loyality in the void.").define("tridentLoyalityVoid", true);
         VOID_HEIGHT = builder.comment("Height of void.").define("voidHeight", -32d);
         builder.pop();
