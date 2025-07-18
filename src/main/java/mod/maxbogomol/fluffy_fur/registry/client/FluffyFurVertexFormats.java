8f2dfb91843c4d9e81d7b642b9829560ecea03f4
MaxBogomol <maxglotov159@gmail.com>
2025-07-18 21:23:18 +0500

    usage

--- src/main/java/mod/maxbogomol/fluffy_fur/registry/client/FluffyFurVertexFormats.java
+++ src/main/java/mod/maxbogomol/fluffy_fur/registry/client/FluffyFurVertexFormats.java
@@ -13,11 +13,11 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 public class FluffyFurVertexFormats {
-    public static final VertexFormatElement UV_CAP_DISTORTED = new VertexFormatElement(0, VertexFormatElement.Type.FLOAT, VertexFormatElement.Usage.GENERIC, 4);
-    public static final VertexFormatElement TIME_DISTORTED = new VertexFormatElement(1, VertexFormatElement.Type.FLOAT, VertexFormatElement.Usage.GENERIC, 1);
-    public static final VertexFormatElement TIME_OFFSET_DISTORTED = new VertexFormatElement(2, VertexFormatElement.Type.FLOAT, VertexFormatElement.Usage.GENERIC, 1);
-    public static final VertexFormatElement AMPLIFIER_DISTORTED = new VertexFormatElement(3, VertexFormatElement.Type.FLOAT, VertexFormatElement.Usage.GENERIC, 1);
-    public static final VertexFormatElement OFFSET_DISTORTED = new VertexFormatElement(4, VertexFormatElement.Type.FLOAT, VertexFormatElement.Usage.GENERIC, 1);
+    public static final VertexFormatElement UV_CAP_DISTORTED = new VertexFormatElement(0, VertexFormatElement.Type.FLOAT, VertexFormatElement.Usage.UV, 4);
+    public static final VertexFormatElement TIME_DISTORTED = new VertexFormatElement(1, VertexFormatElement.Type.FLOAT, VertexFormatElement.Usage.UV, 1);
+    public static final VertexFormatElement TIME_OFFSET_DISTORTED = new VertexFormatElement(2, VertexFormatElement.Type.FLOAT, VertexFormatElement.Usage.UV, 1);
+    public static final VertexFormatElement AMPLIFIER_DISTORTED = new VertexFormatElement(3, VertexFormatElement.Type.FLOAT, VertexFormatElement.Usage.UV, 1);
+    public static final VertexFormatElement OFFSET_DISTORTED = new VertexFormatElement(4, VertexFormatElement.Type.FLOAT, VertexFormatElement.Usage.UV, 1);
 
     public static final VertexFormat TRANSLUCENT = new VertexFormat(ImmutableMap.<String, VertexFormatElement>builder()
             .put("Position", DefaultVertexFormat.ELEMENT_POSITION).put("Color", DefaultVertexFormat.ELEMENT_COLOR).put("UV2", DefaultVertexFormat.ELEMENT_UV2).build());
