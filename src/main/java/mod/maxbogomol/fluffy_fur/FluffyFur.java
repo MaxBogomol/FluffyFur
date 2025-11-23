51f4bd8d0a311d89b60fb8b44a5e8b8c7bded480
MaxBogomol <maxglotov159@gmail.com>
2025-11-23 14:48:39 +0500

    fix

--- src/main/java/mod/maxbogomol/fluffy_fur/FluffyFur.java
+++ src/main/java/mod/maxbogomol/fluffy_fur/FluffyFur.java
@@ -39,8 +39,8 @@ import java.util.Random;
 public class FluffyFur {
     public static final String MOD_ID = "fluffy_fur";
     public static final String NAME = "Fluffy Fur";
-    public static final String VERSION = "0.2.3";
-    public static final int VERSION_NUMBER = 14;
+    public static final String VERSION = "0.2.4";
+    public static final int VERSION_NUMBER = 15;
 
     public static final ISidedProxy proxy = DistExecutor.unsafeRunForDist(() -> ClientProxy::new, () -> ServerProxy::new);
     public static final Logger LOGGER = LogManager.getLogger();
