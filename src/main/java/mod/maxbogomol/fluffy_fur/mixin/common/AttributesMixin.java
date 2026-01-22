96c8cebad32865d883cebfed262d33ba933f4cb7
MaxBogomol <maxglotov159@gmail.com>
2026-01-22 13:55:13 +0500

    FUCKING FIX

--- src/main/java/mod/maxbogomol/fluffy_fur/mixin/common/AttributesMixin.java
+++ src/main/java/mod/maxbogomol/fluffy_fur/mixin/common/AttributesMixin.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.Slice;
 @Mixin(Attributes.class)
 public abstract class AttributesMixin {
 
-    @ModifyArg(method = "<clinit>", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/ai/attributes/RangedAttribute;<init>(Ljava/lang/String;DDD)V", ordinal = 0), slice = @Slice(from = @At(value = "CONSTANT", args = "stringValue=attribute.name.generic.armor")), index = 2)
+    @ModifyArg(method = "<clinit>", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/ai/attributes/RangedAttribute;<init>(Ljava/lang/String;DDD)V", ordinal = 0), slice = @Slice(from = @At(value = "CONSTANT", args = "stringValue=attribute.name.generic.armor")), index = 3)
     private static double fluffy_fur$modifyArmor(double max) {
         if (max == 30.0D) return 100.0D;
         return max;
